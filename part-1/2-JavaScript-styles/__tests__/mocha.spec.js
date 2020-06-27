1e4b0b1ab488cb64fe38e4ca1ad2cacb19950b04
saikrishna321 <saikrishna321@yahoo.com>
2020-06-27 08:54:27 +0530

    initial test

--- part-1/2-JavaScript-styles/__tests__/mocha.spec.js
+++ part-1/2-JavaScript-styles/__tests__/mocha.spec.js
@@ -1,44 +1,82 @@
-before('Root Before', () => {
-    // runs once before all the describes
-    console.log('Root Before...')
-})
+import fetch from 'node-fetch';
 
-describe('Login', () => {
-  describe('User with valid login', () => {
-    before(function() {
-      // runs once before the first test in this block
-      console.log('Before');
+describe('Movie Details', () => {
+  it('User Should be able to see the Movie details', async () => {
+    // TODO Get the movies from http://www.omdbapi.com/?s=MovieName&apikey=******&page=1&type="movie"&plot=full
+    const movies = await fetch(
+      'http://www.omdbapi.com/?s=Press+1+for+Tamil&apikey=c9079b0f&page=1&type="movie"&plot=full'
+    );
+    // TODO Get the series from http://www.omdbapi.com/?s=SeriesName&apikey=****&page=1&type="series"&plot=full
+    const series = await fetch(
+      'http://www.omdbapi.com/?s=Tamil&apikey=c9079b0f&page=1&type="series"&plot=full'
+    );
+
+    // TODO Get the response and Assert
+
+    const movieResponse = await movies.json();
+    const seriesResponse = await series.json();
+
+    // TODO Find movies release after 2012
+    const foundMovies = movieResponse.Search.filter((item, index, array) => {
+      return parseInt(item.Year) > 2012; //tell about +
     });
 
-    after(function() {
-      // runs once after the last test in this block
-      console.log('After');
+    // TODO Find series release after 2012
+    const foundSeries = seriesResponse.Search.filter((item) => {
+      return parseInt(item.Year) > 2012; // tell about +
     });
 
-    beforeEach(function() {
-      // runs before each test in this block
-      console.log('Before Each');
+    // TODO Create a Map with Series and Movies
+    let moviesMap = new Map();
+    moviesMap.set('series', foundSeries);
+    moviesMap.set('movies', foundMovies);
+    // TODO Get Movie details from using the imdbID http://www.omdbapi.com/?i=id&apikey= and Assert
+
+    const seriesList = moviesMap.get('series');
+
+    // Async in ForLoop
+    await asyncForEach(seriesList, async (el) => {
+      const movieDetail = await fetch(
+        `http://www.omdbapi.com/?i=${el.imdbID}&apikey=c9079b0f`
+      );
+      await movieDetail.json();
     });
 
-    afterEach(function() {
-      // runs after each
-      console.log('After Each');
+    //ToDO Promise All
+    let seriesRequests = [];
+    seriesList.forEach((el) => {
+      seriesRequests.push(
+        getMovieData(`http://www.omdbapi.com/?i=${el.imdbID}&apikey=c9079b0f`)
+      );
     });
+    const movieDetailsResults = await Promise.all(seriesRequests).then(
+      (data) => {
+        return data;
+      }
+    );
 
-    it('Should be able to see the detail screen', function() {});
+    // TODO Assign default values to Ratings when its empty and Assert the default values are added to the Ratings
+    let defaultValues = {};
 
-    it('Should be able to search from detail screen', function() {});
-  });
-
-  describe('User with invalid login', () => {
-    it('Should not be able to see the detail screen', function() {});
-
-    it('Should not be able to search from detail screen', function() {});
+    movieDetailsResults.forEach((detail) => {
+      let [prop] = detail.Ratings;
+      if (prop === undefined) prop = Object.assign({}, defaultValues, prop);
+      let { Source = 'Internet Movie Database', Value = 'N/A' } = prop;
+      console.log(Source, Value);
+    });
   });
 });
 
-describe('Logout', () => {
-  describe('User with valid login', () => {
-    it('Should be able logout for application', function() {});
+async function asyncForEach(array, callback) {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+}
+
+function getMovieData(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => resolve(data));
   });
-});
+}
